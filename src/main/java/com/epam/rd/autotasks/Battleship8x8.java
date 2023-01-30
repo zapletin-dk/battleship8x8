22a087a55ccd1cd999a7ee1446d9f508ad9ef638
Zapletin Daniil <Zaplot1994@gmail.com>
2023-01-31 01:06:14 +0300

    Task done with all tests passed

--- src/main/java/com/epam/rd/autotasks/Battleship8x8.java
+++ src/main/java/com/epam/rd/autotasks/Battleship8x8.java
@@ -5,14 +5,41 @@ public class Battleship8x8 {
     private long shots = 0L;
 
     public Battleship8x8(final long ships) {
+
         this.ships = ships;
     }
 
     public boolean shoot(String shot) {
-        throw new UnsupportedOperationException();
+        long tempMask = 0b10000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000L;
+        tempMask >>>= ((int) shot.charAt(0) - 65) + (Character.getNumericValue(shot.charAt(1)) - 1) * 8L;
+        shots |= tempMask;
+        return ships == (ships | tempMask);
     }
 
+
     public String state() {
-        throw new UnsupportedOperationException();
+        StringBuilder sb = new StringBuilder();
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i< 64 ; i++){
+            if (longToBinaryString(shots).charAt(i) == '1' && longToBinaryString(ships).charAt(i) == '1'){
+                sb.append('☒');
+            } else if (longToBinaryString(shots).charAt(i) == '1' && longToBinaryString(ships).charAt(i) == '0'){
+                sb.append('×');
+            } else if (longToBinaryString(shots).charAt(i) == '0' && longToBinaryString(ships).charAt(i) == '1'){
+                sb.append('☐');
+            } else if (longToBinaryString(shots).charAt(i) == '0' && longToBinaryString(ships).charAt(i) == '0'){
+                sb.append('.');
+            }
+        }
+        for (int i = 0; i < 7; i++) {
+            res.append(sb.substring(i * 8, (i + 1) * 8)).append('\n');
+        }
+        res.append(sb.substring(56));
+        return res.toString();
     }
+
+    public static String longToBinaryString(long input) {
+        return String.format("%64s", Long.toBinaryString(input)).replace(' ', '0');
+    }
+
 }
