ff69fa1cac4da56e8d63d07e0d71c8ce6cab53fc
Zapletin Daniil <Zaplot1994@gmail.com>
2023-03-10 14:31:55 +0300

    Code refactored

--- src/main/java/com/epam/rd/autotasks/Battleship8x8.java
+++ src/main/java/com/epam/rd/autotasks/Battleship8x8.java
@@ -5,21 +5,19 @@ public class Battleship8x8 {
     private long shots = 0L;
 
     public Battleship8x8(final long ships) {
-
         this.ships = ships;
     }
 
     public boolean shoot(String shot) {
-        long tempMask = 0b10000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000L;
-        tempMask >>>= ((int) shot.charAt(0) - 65) + (Character.getNumericValue(shot.charAt(1)) - 1) * 8L;
-        shots |= tempMask;
-        return ships == (ships | tempMask);
+        long shootsMask = 0b10000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000L;
+        shootsMask >>>= ((int) shot.charAt(0) - 65) + (Character.getNumericValue(shot.charAt(1)) - 1) * 8L;
+        shots |= shootsMask;
+        return ships == (ships | shootsMask);
     }
-
 
     public String state() {
         StringBuilder sb = new StringBuilder();
-        StringBuilder res = new StringBuilder();
+        StringBuilder state = new StringBuilder();
         for (int i = 0; i< 64 ; i++){
             if (longToBinaryString(shots).charAt(i) == '1' && longToBinaryString(ships).charAt(i) == '1'){
                 sb.append('☒');
@@ -32,10 +30,10 @@ public class Battleship8x8 {
             }
         }
         for (int i = 0; i < 7; i++) {
-            res.append(sb.substring(i * 8, (i + 1) * 8)).append('\n');
+            state.append(sb.substring(i * 8, (i + 1) * 8)).append('\n');
         }
-        res.append(sb.substring(56));
-        return res.toString();
+        state.append(sb.substring(56));
+        return state.toString();
     }
 
     public static String longToBinaryString(long input) {
